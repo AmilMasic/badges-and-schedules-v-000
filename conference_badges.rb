86353de5df4a9978d6b5bee4ebfefd72a5c79c2d
Amil Masic <amil.masic@gmail.com>
2018-09-11 14:06:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,11 @@
 # Write your code here.
-  # names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(badges)
-    badges.each do |name|
-      puts "Hello, my name is #{name}."
+def batch_badge_creator(names)
+    names.each do |name|
+      
     end
 end
